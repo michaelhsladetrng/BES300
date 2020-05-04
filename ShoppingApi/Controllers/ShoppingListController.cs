05e2cdbfd8290a8d13e56e3616d1f26cce5fd101
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2020-05-04 17:45:06 +0000

    Add database connection

--- ShoppingApi/Controllers/ShoppingListController.cs
+++ ShoppingApi/Controllers/ShoppingListController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApi.Data;
 using ShoppingApi.Models;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,25 @@ namespace ShoppingApi.Controllers
 {
     public class ShoppingListController : ControllerBase
     {
+        private readonly ShoppingDataContext DataContext;
+
+        public ShoppingListController(ShoppingDataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
         [HttpGet("shoppinglist")]
         public  async Task<ActionResult> GetFullShoppingList()
         {
             var response = new GetShoppingListResponse();
+
+            response.Data = await DataContext.ShoppingItems
+                .Select(item => new ShoppingListItemResponse
+                {
+                    Id = item.Id,
+                    Description = item.Description,
+                    Purchased = item.Purchased
+                }).ToListAsync();
 
             return Ok(response);
         }
